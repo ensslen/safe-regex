d6a22c375fc87a5cbd0cacc7f2667dfdc4443178
Steven Ensslen <steven@claritycloudworks.com>
2020-11-19 14:46:00 +1300

    adds RegularExpression class

--- safe_regex/dataclasses.py
+++ safe_regex/dataclasses.py
@@ -1,10 +1,68 @@
 import pydantic
+import os
+import yaml
+import re
 
 
-class SafeRegexAbstractModel(pydantic.BaseModel):
+@pydantic.dataclasses.dataclass()
+class RegularExpression:
+    pattern: pydantic.constr(min_length=2)
+    matching_texts: pydantic.conset(item_type=str)
+    non_matching_texts: pydantic.conset(item_type=str)
+
+    @classmethod
+    def from_yaml(cls, expression_name: str, folder: str = None):
+        environment_path = os.environ.get("SAFE_REGEX_PATH")
+        if folder:
+            working_folder = folder
+        elif environment_path:
+            working_folder = environment_path
+        else:
+            working_folder = os.getcwd()
+        file_path = os.path.join(working_folder, f"{expression_name}.re.yaml")
+        with open(file_path, "r") as yaml_file:
+            yaml_data = yaml.safe_load(yaml_file)
+        return cls(**yaml_data)
+
+    def __post_init_post_parse__(self):
+        self.regex = re.compile(self.pattern)
+        self.flags = self.regex.flags
+        self.groups = self.regex.groups
+        self.groupindex = self.regex.groupindex
+
     class Config:
         extra = "forbid"
 
+    def test(self):
+        for text in self.matching_texts:
+            assert isinstance(self.match(text), re.Match), f"{text} does not match {self.regex}"
+        for text in self.non_matching_texts:
+            assert self.match(text) is None, f"{text} should not match {self.regex}"
 
-class SafeRegex(SafeRegexAbstractModel):
-    unique_list: pydantic.conset(item_type=str)
+    """
+    pass through to re.Pattern
+    """
+
+    def search(self, *args, **kwargs):
+        return self.regex.search(*args, **kwargs)
+
+    def match(self, *args, **kwargs):
+        return self.regex.match(*args, **kwargs)
+
+    def fullmatch(self, *args, **kwargs):
+        return self.regex.fullmatch(*args, **kwargs)
+
+    def split(self, *args, **kwargs):
+        return self.regex.split(*args, **kwargs)
+
+    def findall(self, *args, **kwargs):
+        return self.regex.findall(*args, **kwargs)
+
+    def finditer(self, *args, **kwargs):
+        return self.regex.finditer(*args, **kwargs)
+
+    def sub(self, *args, **kwargs):
+        return self.regex.sub(*args, **kwargs)
+
+    def subn(self, *args, **kwargs):
+        return self.regex.subn(*args, **kwargs)
--- tests/test_dataclasses.py
+++ tests/test_dataclasses.py
@@ -1,13 +1,82 @@
-from safe_regex.dataclasses import SafeRegex
+from safe_regex.dataclasses import RegularExpression
 import pytest
 import pydantic
+import yaml
+import re
 
 
 def test_dataclass():
-    with pytest.raises(pydantic.error_wrappers.ValidationError):
-        SafeRegex(cats=1, unique_list=["a"])
+    with pytest.raises(TypeError):
+        RegularExpression(cats=1, regex="abc")
 
 
-def test_unique_list():
-    sr = SafeRegex(unique_list=[1, 1])
+@pytest.fixture
+def example():
+    return RegularExpression.from_yaml("yyyy-mm-dd", folder="./tests")
+
+
+def test_from_yaml(example):
+    # the test fixture does this for us
     pass
+
+
+def test_testing():
+    with open("./tests/yyyy-mm-dd.re.yaml") as yaml_file:
+        yaml_data = yaml.safe_load(yaml_file)
+    sr = RegularExpression(**yaml_data)
+    sr.test()
+
+
+def test_search(example):
+    assert isinstance(example.search("the date was 2020-11-19", 5), re.Match)
+
+
+def test_match(example):
+    assert isinstance(example.match("1111-11-11 was the date", 0, 15), re.Match)
+
+
+def test_fullmatch(example):
+    assert isinstance(example.fullmatch("1111-11-11 was the date", 0, 10), re.Match)
+
+
+def test_split(example):
+    assert example.split("it started 2020-01-01 and ended 2020-02-02", maxsplit=4) == [
+        "it started ",
+        " and ended ",
+        "",
+    ]
+
+
+def test_findall(example):
+    assert example.findall("it started 2020-01-01 and ended 2020-02-02", 6) == [
+        "2020-01-01",
+        "2020-02-02",
+    ]
+
+
+def test_finditer(example):
+    assert list(example.findall("the dates where 2020-02-02, 2020-03-03, and 2020-04-04", 6)) == [
+        "2020-02-02",
+        "2020-03-03",
+        "2020-04-04",
+    ]
+
+
+def test_sub(example):
+    assert example.sub("yesterday", "it started 2020-01-01") == "it started yesterday"
+
+
+def test_subn(example):
+    assert example.subn("yesterday", "it started 2020-01-01") == ("it started yesterday", 1)
+
+
+def test_flags(example):
+    assert example.flags == re.UNICODE
+
+
+def test_groups(example):
+    assert example.groups == 0
+
+
+def test_groupindex(example):
+    assert example.groupindex == {}
